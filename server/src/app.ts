b29170d455485f5efbaedb66ff5d973b0037bd71
Fadi <fadih1992@gmail.com>
2026-02-16 16:57:33 -0800

    update

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
-import cors from "cors";
+import userRoutes from "./routes/userRoutes";
 
 const app = express();
 
-// Middleware
-app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
 });
+
+app.use("/api/users", userRoutes);
 
 export default app;
