0eff72bb87a244ba3b3f5c9480371aa0cd40e0c1
Fadi <fadih1992@gmail.com>
2026-02-20 21:54:19 -0800

    update

---
+++ server/src/models/Listing.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const listingSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    location: {
+      type: String,
+      required: true,
+    },
+
+    images: [
+      {
+        type: String,
+      },
+    ],
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+export default Listing;
