3dbcbbc9879f1dd90cd7fa856f819fceac9ce803
Fadi <fadih1992@gmail.com>
2026-02-23 21:08:58 -0800

    update

---
+++ server/src/middleware/uploadMiddleware.ts
@@ -0,0 +1,35 @@
+import multer from "multer";
+import path from "path";
+
+// Storage config
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads/"); // folder
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+// File filter (images only)
+function checkFileType(file: Express.Multer.File, cb: multer.FileFilterCallback) {
+  const filetypes = /jpg|jpeg|png|webp/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    cb(null, true);
+  } else {
+    cb(new Error("Images only!"));
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => checkFileType(file, cb),
+});
+
+export default upload;
