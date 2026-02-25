e4f2cf7036091580876e492657fb97d0fd5baeff
Fadi <fadih1992@gmail.com>
2026-02-24 21:15:40 -0800

    update

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,8 @@ import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import listingRoutes from "./routes/listingRoutes";
 
+import path from "path";
+
 const app = express();
 
 app.use(express.json());
@@ -11,4 +13,5 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/listings", listingRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 export default app;
