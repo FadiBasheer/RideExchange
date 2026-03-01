2334dcb7f4f5392b583c3cda1a94d0dcceaefde6
Fadi <fadih1992@gmail.com>
2026-02-28 22:59:19 -0800

    update

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -1,15 +1,13 @@
-import jwt from "jsonwebtoken";
+import jwt, { Secret, SignOptions } from "jsonwebtoken";
 
 const generateToken = (id: string) => {
-  const secret = process.env.JWT_SECRET as string;
+  const secret: Secret = process.env.JWT_SECRET as string;
 
-  return jwt.sign(
-    { id },
-    secret,
-    {
-      expiresIn: "1d",
-    }
-  );
+  const options: SignOptions = {
+    expiresIn: process.env.JWT_EXPIRES_IN || "1d",
+  };
+
+  return jwt.sign({ id }, secret, options);
 };
 
 export default generateToken;
