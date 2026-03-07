3408133e61531d5c973130d656b3bbd0631cdcd2
Fadi <fadih1992@gmail.com>
2026-03-07 10:48:16 -0800

    update

---
+++ client/src/pages/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Listings</h1>
+
+    <div v-for="listing in listings" :key="listing._id">
+      <router-link :to="`/listing/${listing._id}`">
+        {{ listing.title }} - ${{ listing.price }}
+      </router-link>
+    </div>
+  </div>
+</template>
